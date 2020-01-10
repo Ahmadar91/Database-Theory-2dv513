326fb351f9d5bf11eec77d7d4d896f148c444d78
Ahamd <aa224fn@student.lnu.se>
2020-01-10 18:07:02 +0100

    server 3

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
@@ -7,6 +8,20 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(
+//   cors({
+//     origin: (origin, cb) => cb(null, true),
+//     credentials: true,
+//     preflightContinue: true,
+//     exposedHeaders: [
+//       'Access-Control-Allow-Headers',
+//       'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept',
+//       'X-Password-Expired'
+//     ],
+//     optionsSuccessStatus: 200
+//   })
+// )
+app.use(cors())
 // db Connection
 const db = require('./database/connection')
 db
@@ -18,6 +33,6 @@ db
     console.error('Unable to connect to the database:', err)
   })
 
-app.get('/', require('./routes/homeRouter'))
+app.get('', require('./routes/homeRouter'))
 
-app.listen(PORT, console.log(`started: listin g on port ${PORT}`))
+app.listen(PORT, console.log(`started: on port ${PORT}`))
